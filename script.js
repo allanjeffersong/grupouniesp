9829d78c31320965626d486b866faa07a8047e82
allanjeffersong <allan012silva@gmail.com>
2024-08-19 15:25:15 -0300

    enviando pastas das disciplinas

---
+++ script.js
@@ -0,0 +1,8 @@
+// Assuming you have an HTML element with the id "botId"
+const botId = document.getElementById('botId');
+
+// Add a click event listener to the botId element
+botId.addEventListener('click', function() {
+  // Display an alert with the text "olaa"
+  alert('está funcionando');
+});
